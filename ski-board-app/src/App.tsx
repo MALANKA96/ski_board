d2197553853d019c6f36ddcc9e45172005e63228
Николай Вокса <@bkp.local>
2021-11-18 14:52:18 +0300

    add blabla

--- ski-board-app/src/App.tsx
+++ ski-board-app/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo1 from './logoSki.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         init app ski & board
         </p>
         <a
           className="App-link"
